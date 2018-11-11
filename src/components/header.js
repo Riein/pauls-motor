1389c1d20fe84cd83c6649491b3972f6fe90708f
Sasha Demyanik <ttsvan@msn.com>
2018-11-10 17:58:55 -0800

    Sets up header css.

--- src/components/header.js
+++ src/components/header.js
@@ -1,47 +1,38 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import './header.module.css'
+import styles from './header.module.css'
 
 const Header = () => (
-  <div className="navigation">
-    <h1 style={{ margin: 0 }}>
-      <Link
-        to="/"
-        style={{
-          color: 'white',
-          textDecoration: 'none',
-        }}
-      >
-      </Link>
-    </h1>
-
-    <ul>
-      <li><Link to="/" 
-          style={{
-            color: 'black',
-            textDecoration: 'none',
-        }}
-        >Home</Link></li>
-      <li><Link to="/services" 
-          style={{
-            color: 'black',
-            textDecoration: 'none',
-        }}
-        >Services</Link></li>
-      <li id="logo">Paul's Motor Inc</li>
-      <li><Link to="/about" 
-          style={{
-            color: 'black',
-            textDecoration: 'none',
-        }}
-        >About</Link></li>
-      <li><Link to="/contact-us" 
-          style={{
-            color: 'black',
-            textDecoration: 'none',
-        }}
-        >Contact Us</Link></li>
-    </ul>
+  <div className={styles.navigationOuter}>
+    <div className={styles.navigationInner}>
+      <ul>
+        <li><Link to="/" 
+            style={{
+              color: 'black',
+              textDecoration: 'none',
+          }}
+          >Home</Link></li>
+        <li><Link to="/services" 
+            style={{
+              color: 'black',
+              textDecoration: 'none',
+          }}
+          >Services</Link></li>
+        <li className={styles.logo}>Paul's Motor Inc</li>
+        <li><Link to="/about" 
+            style={{
+              color: 'black',
+              textDecoration: 'none',
+          }}
+          >About</Link></li>
+        <li><Link to="/contact-us" 
+            style={{
+              color: 'black',
+              textDecoration: 'none',
+          }}
+          >Contact Us</Link></li>
+        </ul>
+    </div>
   </div>
 )
 

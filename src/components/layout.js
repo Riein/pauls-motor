b8ca4608e3e6698d898c3b47f48ad41db90452e4
Sasha Demyanik <ttsvan@msn.com>
2018-11-12 15:26:29 -0800

    Adds footer component and styling.

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import Cover from './cover'
+import Footer from './footer'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -35,6 +36,7 @@ const Layout = ({ children }) => (
         >
           {children}
         </div>
+        <Footer />
       </>
     )}
   />

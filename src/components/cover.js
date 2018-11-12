2ed489ae8fd65cd245edf94f2b3cd7a854f660fa
Sasha Demyanik <ttsvan@msn.com>
2018-11-12 14:57:54 -0800

    Finishes cover styling and content.

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'gatsby'
 import styles from './cover.module.css'
 
 const Cover = () => (
@@ -6,6 +7,13 @@ const Cover = () => (
         <div className={styles.coverContent} >
             <h1>Quality Auto Repair.</h1>
             <h2>Offering high-quality auto repair and used car sales.</h2>
+            <h2>
+                <Link to='/services' 
+                style={{ 
+                    color: 'white', 
+                    }}
+                >Click here to find out more!</Link>
+            </h2>
         </div>
     </div>
 )

67a0941536e202e0d22cd524ce29976ff0634a8e
Sasha Demyanik <ttsvan@msn.com>
2018-11-13 15:27:12 -0800

    Adds contact us page content.
    Basic contact information provided but additional information is needed.

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -4,7 +4,18 @@ import Layout from '../components/layout'
 
 const ContactUsPage = () => (
   <Layout>
-    
+    <div className="section1">
+
+      <div className="section1Header">
+        <h1>Contact Us</h1>
+      </div>
+
+      <div className="contact-details">
+        <h2>Number: (360) 694-0431</h2>
+        <h2>Location: 4703 NE Hazel Dell Ave, Vancouver, WA, 98663</h2>
+        <h2>Hours: Monday to Friday - 9AM to 5PM</h2>
+      </div>
+    </div>
   </Layout>
 )
 

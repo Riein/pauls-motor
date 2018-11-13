75e56d1569619d1eadb2a4f87b48c3e77ac188a6
Sasha Demyanik <ttsvan@msn.com>
2018-11-13 12:47:08 -0800

    Fills out about page content and styling.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,25 @@ import Layout from '../components/layout'
 
 const AboutPage = () => (
   <Layout>
-    
+    <div className="section1">
+
+      <div className="section1Header">
+        <h1>About Paul's Motor Inc</h1>
+      </div>
+
+      <div className="section1Body">
+        <h2>History</h2>
+        <p>Paul is originally from Ukraine but has been living
+          in the United States since 1991. He and his family
+          moved to Vancouver in 1994, where he opened his business.
+          While initially specializing in Jeeps, Paul has worked
+          with Volkswagen, Honda, Toyota, Ford, Chevy, Subaru, Kia,
+          Mazda, and many more car makes.
+          Aside from his business, Paul is active in the Ukrainian
+            Christian community and likes to spend time with his family.
+        </p>
+      </div>
+    </div>
   </Layout>
 )
 
